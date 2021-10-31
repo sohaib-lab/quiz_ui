db1b4bd9556973b97c108698e465c4bd125b0891
sohaib-lab <sheikhsohaib0335@gmail.com>
2021-11-01 00:17:16 +0500

    first commit

--- lib/data/questions_exam.dart
+++ lib/data/questions_exam.dart
@@ -28,25 +28,33 @@ List<QuestionModel> questions = [
     "John":false,
     "Dominic":false,
     "Bilius": true,
+
+    
     
   }),
-  QuestionModel("From which album is the Gorillaz song, \"On Melancholy Hill\" featured in?", {
 
-    "Plastic Beach": true,
-    "Demon Days":false,
-    "Humanz":false,
-    "The Fall":false,
 
-  }),
- 
-  QuestionModel(
+ QuestionModel(
+      "Who is the creator of the comic series \"The Walking Dead\"?",
+      {
+        "Robert Crumb":false,
+        "Malcolm Wheeler-Nicholson":false,
+        "Stan Lee":false,
+        "Robert Kirkman":true,
+       
+      }),
+
+      
+
+      QuestionModel(
     "At the start of a standard game of the Monopoly, if you throw a double six,which square would you land on?", {
     "Water Works":false,
     "Chance":false,
     "Community Chest":false,
     "Electric Company": true,
   }),
-  QuestionModel("What is the capital of Jamaica ?", {
+
+   QuestionModel("What is the capital of Jamaica ?", {
     
      "Rio de Janeiro":false,
      "Dar es Salaam":false,
@@ -54,6 +62,7 @@ List<QuestionModel> questions = [
      "Kingston":true,
    
   }),
+
   QuestionModel(
       "When did Jamaica recieve its independence from England ?",
       {
@@ -64,15 +73,22 @@ List<QuestionModel> questions = [
        
       }),
 
-      QuestionModel(
-      "Who is the creator of the comic series \"The Walking Dead\"?",
-      {
-        "Robert Crumb":false,
-        "Malcolm Wheeler-Nicholson":false,
-        "Stan Lee":false,
-        "Robert Kirkman":true,
-       
-      }),
+       QuestionModel(
+    "Kangaroos keep food in their pouches next to their children.", {
+    
+     "False":true,
+     "True":false,
+     
+  }),
+
+   QuestionModel(
+     "In 2013 how much money was lost by Nigerian scams?", {
+    
+    "12.7 Billion":true,
+    "95 Million":false,
+    "956 Million":false,
+    "2.7 Billion":false,
+  }),
 
   QuestionModel(
     "How old was Lyndon B. Johnson when he assumed the role of President of the United States ?", {
@@ -83,41 +99,65 @@ List<QuestionModel> questions = [
      "54":false,
   }),
 
-   QuestionModel( "What was the last colony the UK ceded marking the end of the British Empire?", {
-
-    "Hong Kong":true,
-    "India":false,
-    "Australia":false,
-    "Ireland":false,
+   QuestionModel(
+    "In World of Warcraft lore, who organized the creation of the Paladins?", {
+    
+     "Alonsus Faol":true,
+     "Uther the Lightbringer":false,
+     "Alexandros Mograine":false,
+     "Sargeras, The Daemon Lord":false,
   }),
 
-  QuestionModel(
-    "What was the name of the hero in the 80s animated video game 'Dragon's Lair'?",
+  QuestionModel( "In the game \"Subnautica\", a \"Cave Crawler\" will attack you.", {
+
+    "True":true,
+    "False":false,
+    
+  }),
+
+   QuestionModel(
+   "What is the name of the device that allows for infinite energy in the anime \"Dimension W\"?",
     
     {
-      "Arthur":false,
-      "Sir Toby Belch":false,
-      "Guy of Gisbourne":false,
-      "Dirk the Daring": true,
+      "Tesla":false,
+      "Corector":false,
+      "Wires":false,
+      "Coils": true,
      
     },
   ),
-  QuestionModel("What is the scientific name for modern day humans ?", {
 
-    "Homo Ergaster":false,
-    "Homo Erectus":false,
-    "Homo Neanderthalensis": false,
-    "Homo Sapiens": true,
+   QuestionModel( "What is the name of Cream the Rabbit's mom in the \"Sonic the Hedgehog\" series?", {
+
+    "straberry":false,
+    "Peach":false,
+    "Vanilla": true,
+    "mint": false,
   }),
-  
-  QuestionModel("What is Ron Weasley's middle name?", {
 
-    "Arthur":false,
-    "John":false,
-    "Dominic":false,
-    "Bilius": true,
+  QuestionModel("These two countries held a commonwealth from the 16th to 18th century.", {
+
+     "Hutu and Rwanda":false,
+     "North Korea and South Korea":false,
+     "Poland and Lithuania": true,
+     "Bangladesh and Bhutan":false,
+    
     
   }),
+
+  QuestionModel("Which of these voices wasn't a choice for the House AI in \"The Simpsons Treehouse of Horror\" short, House of Whacks?", {
+
+    "Matthew Perry":false,
+    "Dennis Miller":false,
+    "George Clooney" : true,
+    "Pierce Brosnan":false,
+    
+    
+  }),
+
+
+  
+
   QuestionModel("From which album is the Gorillaz song, \"On Melancholy Hill\" featured in?", {
 
     "Plastic Beach": true,
@@ -126,57 +166,30 @@ List<QuestionModel> questions = [
     "The Fall":false,
 
   }),
- 
-  QuestionModel(
-    "At the start of a standard game of the Monopoly, if you throw a double six,which square would you land on?", {
-    "Water Works":false,
-    "Chance":false,
-    "Community Chest":false,
-    "Electric Company": true,
-  }),
-  QuestionModel("What is the capital of Jamaica ?", {
+
+   QuestionModel( "Scotland voted to become an independent country during the referendum from September 2014.", {
+
+    "True":true,
+    "False":false,
     
-     "Rio de Janeiro":false,
-     "Dar es Salaam":false,
-     "Kano":false,
-     "Kingston":true,
-   
-  }),
-  QuestionModel(
-      "When did Jamaica recieve its independence from England ?",
-      {
-        "1492":false,
-        "1963":false,
-        "1987":false,
-        "1962":true,
-       
-      }),
-
-      QuestionModel(
-      "Who is the creator of the comic series \"The Walking Dead\"?",
-      {
-        "Robert Crumb":false,
-        "Malcolm Wheeler-Nicholson":false,
-        "Stan Lee":false,
-        "Robert Kirkman":true,
-       
-      }),
-
-  QuestionModel(
-    "How old was Lyndon B. Johnson when he assumed the role of President of the United States ?", {
-    
-     "55":true,
-     "50":false,
-     "60":false,
-     "54":false,
   }),
 
-   QuestionModel( "What was the last colony the UK ceded marking the end of the British Empire?", {
+  QuestionModel("In Left 4 Dead, which campaign has the protagonists going through a subway in order to reach a hospital for evacuation?", {
 
-    "Hong Kong":true,
+    "No Mercy": true,
+    "Subway Sprint":false,
+    "Hospital Havoc":false,
+    "Blood Harvest":false,
+
+  }),
+
+  QuestionModel( "What was the last colony the UK ceded marking the end of the British Empire?", {
+
+    "Hong Kong" : true,
     "India":false,
     "Australia":false,
     "Ireland":false,
+
   }),
 
 ];
